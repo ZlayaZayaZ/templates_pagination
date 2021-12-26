f88f1bf42a121893da71ff933fe6ddeca33856d2
Lubov Vasilkova <damirana_@mail.ru>
2021-12-26 22:18:35 +0700

    transferring content outside of a function

--- pagination/stations/views.py
+++ pagination/stations/views.py
@@ -4,6 +4,13 @@ from django.core.paginator import Paginator
 
 import csv
 
+CONTENT = []
+with open('data-398-2018-08-30.csv', newline='', encoding='UTF-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        info = {'Name': row['Name'], 'Street': row['Street'], 'District': row['District']}
+        CONTENT.append(info)
+
 
 def index(request):
     return redirect(reverse('bus_stations'))
@@ -11,12 +18,6 @@ def index(request):
 
 def bus_stations(request):
     page_number = int(request.GET.get('page', 1))
-    CONTENT = []
-    with open('data-398-2018-08-30.csv', newline='', encoding='UTF-8') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            info = {'Name': row['Name'], 'Street': row['Street'], 'District': row['District']}
-            CONTENT.append(info)
     paginator = Paginator(CONTENT, 10)
     page = paginator.get_page(page_number)
     paginator_page = paginator.page(page_number)
